48c253c41bc4aa644c4b347690fee362e09b434d
Haniff <kamalhaniff5@gmail.com>
2023-03-30 16:02:50 +0800

    abc.js

---
+++ abc/abc.js
@@ -0,0 +1,28 @@
+let str = "1.258"
+let num = 1
+
+let total = parseFloat(str) + num
+console.log(total, typeof total)
+
+let data = ["benr", 3523, true]
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+let user = {
+    name : "gua",
+    age: 22,
+    faculty: "FKEKK",
+}
+console.log(user.name)
+console.log(user.age)
+console.log(user.faculty)
+
+let messages = [
+    {name:"usr1",date:"2/4/2023",msg: "very good video",like: 1000},
+    {name:"usr2",date:"2/4/2023",msg: "very good video",like: 2000},
+    {name:"usr3",date:"2/4/2023",msg: "very good video",like: 3000}
+]
+
+console.log(messages[1].msg)
